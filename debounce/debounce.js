073bbccab87e888796f23135e1db078ed9b688b7
liyang <liyang@xiaobu121.com>
2020-11-03 11:48:15 +0800

    防抖和节流

--- debounce/debounce.js
+++ debounce/debounce.js
@@ -1,14 +1,41 @@
+// function db(fn, wait = 400, immediate = false) {
+//   console.log()
+//   let timer = null
+//   return function () {
+//     clearTimeout(timer)
+//     // 判断是否要立即执行一次 
+//     if (immediate && !timer) {
+//       fn.call(this, arguments)
+//     }
+//     timer = setTimeout(() => {
+//       fn.call(this, arguments)
+//     }, wait);
+//   }
+// }
+
+
 function db(fn, wait = 400, immediate = false) {
-  console.log()
-  let timer = null
+  let timer = null;
+  console.log(timer, 'db')
   return function () {
+    console.log(timer, 'anonymous')
     clearTimeout(timer)
-    // 判断是否要立即执行一次 
     if (immediate && !timer) {
       fn.call(this, arguments)
     }
     timer = setTimeout(() => {
       fn.call(this, arguments)
     }, wait);
+  }
+}
+
+
+
+function debounce(fn, wait = 400) {
+  let timer = null;
+  return function () {
+    console.log(timer)
+    clearTimeout(timer)
+    timer = setTimeout(fn, wait)
   }
 }
